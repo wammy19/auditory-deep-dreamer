126f5751707efca532d08b8c0f29f8642dd3f1f2
wammy19 <andrea.spiteri19@gmail.com>
2021-11-24 10:50:43 +0000

    Improved mp3 to wav converter script.

--- scripts/batch-conv-mp3-to-wav.sh
+++ scripts/batch-conv-mp3-to-wav.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-# Scripts created to convert the philharmonia instrument data-set from mp3 format to wav files so liberos can load them.
+# Recursively loop through directories and convert mp3's into wav files using sox.
 
 # Dependencies:
 # sox
 
-# Learning resources:
-# 1. https://unix.stackexchange.com/questions/171677/cd-into-all-directories-execute-command-on-files-in-that-directory-and-return/171679
-# 2. https://unix.stackexchange.com/questions/425532/batch-convert-mp3-files-to-wav-using-sox
+find . -name "*.mp3" -type f -exec sox {} "$(basename {} .mp3).wav" \; -exec rm {} \;
 
-for dir in ./*; do
-  if [ -d "$dir" ]; then
-    cd "$dir"
-    for subDir in ./*; do
-      if [ -d "$subDir" ]; then
-        cd "$subDir"
-        for i in *.mp3; do
-          sox "$i" "$(basename "$i" .mp3).wav"
-          rm "$i"
-        done
-        cd ..
-      fi
-    done
-    cd ..
-  fi
-done
+
+  mv "$file" "${file%.html}.txt"
