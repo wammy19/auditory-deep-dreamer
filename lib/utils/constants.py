33962cedfb6e141cb8e7038b018ec67ba27b492b
wammy19 <andrea.spiteri19@gmail.com>
2021-12-11 14:56:14 +0000

    Sync laptop work to desktop

--- lib/utils/constants.py
+++ lib/utils/constants.py
@@ -1,6 +1,12 @@
 # Audio settings.
+# General
 SAMPLE_RATE: int = 22_050
+
+# Mel Spec.
 MEL_HOP_LEN: int = 512
 NUM_MELS: int = 128
 NUM_FFT: int = 2_048
 MEL_TO_AUDIO_N_ITERATIONS: int = 32
+
+# Pre Processing.
+TRIM_DB: int = 25
