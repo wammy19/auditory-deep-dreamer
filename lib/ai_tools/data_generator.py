3e4bda82227f523bda403a7394e3aad730647303
Andrea Spiteri <aspit002@gold.ac.uk>
2021-12-22 11:19:26 +0000

    Commit so i can work in bed :3

--- lib/ai_tools/data_generator.py
+++ lib/ai_tools/data_generator.py
@@ -5,6 +5,7 @@ import json
 import numpy as np
 import os
 from typing import List, Tuple
+import utils.constants as consts
 
 
 class DataGenerator(Sequential):
@@ -14,28 +15,20 @@ class DataGenerator(Sequential):
 
     def __init__(
             self,
-            list_IDs: List,
+            wav_paths: str,
             labels: List[int],
             batch_size: int = 32,
             dim: Tuple = (32, 32, 32),
             n_channels: int = 1,
             n_classes: int = 10,
+            sample_rate: int = consts.SAMPLE_RATE,
             shuffle: bool = True
     ):
-        """
-        :param list_IDs:
-        :param labels:
-        :param batch_size:
-        :param dim:
-        :param n_channels:
-        :param n_classes:
-        :param shuffle:
-        """
 
         super().__init__()
 
         # Data.
-        self.list_IDs = list_IDs
+        self.wav_paths = wav_paths
         self.labels = labels
 
         # Parameters
@@ -45,16 +38,17 @@ class DataGenerator(Sequential):
         self.n_classes = n_classes
         self.shuffle = shuffle
         self.indexes: np.ndarray = np.array([])
+        self.sample_rate = sample_rate
         self.on_epoch_end()
 
 
-    def __len__(self):
+    def __len__(self) -> int:
         """
         :param self:
         :return:
         """
 
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
+        return int(np.floor(len(self.wav_paths) / self.batch_size))
 
 
     def __getitem__(self, index):
@@ -67,10 +61,10 @@ class DataGenerator(Sequential):
         indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
 
         # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        wav_paths = [self.list_IDs[k] for k in indexes]
 
         # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
+        X, y = self.__data_generation(wav_paths)
 
         return X, y
 
@@ -86,9 +80,9 @@ class DataGenerator(Sequential):
             np.random.shuffle(self.indexes)
 
 
-    def __data_generation(self, list_IDs_temp):
+    def __data_generation(self, wav_paths) -> Tuple[np.ndarray, np.ndarray]:
         """
-        :param list_IDs_temp:
+        :param wav_paths:
         :return:
         """
 
@@ -97,7 +91,7 @@ class DataGenerator(Sequential):
         y = np.empty(self.batch_size, dtype=int)
 
         # Generate data
-        for i, ID in enumerate(list_IDs_temp):
+        for i, ID in enumerate(wav_paths):
             # Store sample
             X[i,] = np.load('data/' + ID + '.npy')
 
---
+++ lib/ai_tools/examples.py
@@ -0,0 +1,54 @@
+from kapre.composed import get_melspectrogram_layer
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, MaxPooling2D, Flatten
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.activations import relu, sigmoid
+from tensorflow.keras.optimizers import Adam
+from typing import Tuple
+import utils.constants as consts
+
+
+def build_simple_cnn() -> Sequential:
+    """
+    :return: A compiled tensorflow.keras.Model ready for fitting.
+    """
+
+    # Shape: (num_samples, sample_rate, channels)
+    input_shape: Tuple[int, int] = (consts.SAMPLE_RATE, 1)  # Mono audio so 1 channel.
+
+    # First layer transforms raw audio into a mel-spectrogram in GPU.
+    mel_layer: Sequential = get_melspectrogram_layer(
+        input_shape=input_shape,
+        n_fft=consts.NUM_FFT,
+        hop_length=consts.MEL_HOP_LEN,
+        sample_rate=consts.SAMPLE_RATE,
+        return_decibel=True,
+        input_data_format='channels_last',
+        output_data_format='channels_last'
+    )
+
+    model = Sequential()
+
+    # Mel transformation.
+    model.add(mel_layer)
+
+    # Conv block 1.
+    model.add(Conv2D(
+        32,
+        (3, 3),
+        strides=(1, 1),
+        activation=relu
+    ))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    # Flatten and a binary output for final layer.
+    model.add(Flatten())
+    model.add(Dense(1, activation=sigmoid))
+
+    model.compile(
+        loss=BinaryCrossentropy(),
+        optimizer=Adam(),
+        metrics=['accuracy']
+    )
+
+    return model
