5acd187f5d31e7950f16b89234f4b4b612c4cfa7
Andrea Spiteri <aspit002@gold.ac.uk>
2021-12-22 11:21:50 +0000

    Revert "Commit so i can work in bed :3"
    This reverts commit b8b50998cadc1692195344d0fc56689b40b57846.

--- lib/ai_tools/data_generator.py
+++ lib/ai_tools/data_generator.py
@@ -5,7 +5,6 @@ import json
 import numpy as np
 import os
 from typing import List, Tuple
-import utils.constants as consts
 
 
 class DataGenerator(Sequential):
@@ -15,20 +14,28 @@ class DataGenerator(Sequential):
 
     def __init__(
             self,
-            wav_paths: str,
+            list_IDs: List,
             labels: List[int],
             batch_size: int = 32,
             dim: Tuple = (32, 32, 32),
             n_channels: int = 1,
             n_classes: int = 10,
-            sample_rate: int = consts.SAMPLE_RATE,
             shuffle: bool = True
     ):
+        """
+        :param list_IDs:
+        :param labels:
+        :param batch_size:
+        :param dim:
+        :param n_channels:
+        :param n_classes:
+        :param shuffle:
+        """
 
         super().__init__()
 
         # Data.
-        self.wav_paths = wav_paths
+        self.list_IDs = list_IDs
         self.labels = labels
 
         # Parameters
@@ -38,17 +45,16 @@ class DataGenerator(Sequential):
         self.n_classes = n_classes
         self.shuffle = shuffle
         self.indexes: np.ndarray = np.array([])
-        self.sample_rate = sample_rate
         self.on_epoch_end()
 
 
-    def __len__(self) -> int:
+    def __len__(self):
         """
         :param self:
         :return:
         """
 
-        return int(np.floor(len(self.wav_paths) / self.batch_size))
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
 
 
     def __getitem__(self, index):
@@ -61,10 +67,10 @@ class DataGenerator(Sequential):
         indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
 
         # Find list of IDs
-        wav_paths = [self.list_IDs[k] for k in indexes]
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
 
         # Generate data
-        X, y = self.__data_generation(wav_paths)
+        X, y = self.__data_generation(list_IDs_temp)
 
         return X, y
 
@@ -80,9 +86,9 @@ class DataGenerator(Sequential):
             np.random.shuffle(self.indexes)
 
 
-    def __data_generation(self, wav_paths) -> Tuple[np.ndarray, np.ndarray]:
+    def __data_generation(self, list_IDs_temp):
         """
-        :param wav_paths:
+        :param list_IDs_temp:
         :return:
         """
 
@@ -91,7 +97,7 @@ class DataGenerator(Sequential):
         y = np.empty(self.batch_size, dtype=int)
 
         # Generate data
-        for i, ID in enumerate(wav_paths):
+        for i, ID in enumerate(list_IDs_temp):
             # Store sample
             X[i,] = np.load('data/' + ID + '.npy')
 
