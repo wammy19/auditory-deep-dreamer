3e2bc6681b6a201884cf156dbf1447a31552c341
Andrea Spiteri <aspit002@gold.ac.uk>
2022-02-04 15:54:08 +0000

    Created a script that renames all the nsynth files to have a note.

--- scripts/rename_nsynth_dataset.py
+++ scripts/rename_nsynth_dataset.py
@@ -1,13 +1,38 @@
 import os
+from os.path import join
 import re
+from typing import List
 from utils.midi_note_table import MidiNoteTable
 
 
-def main():
-    # nsynth_midi_note_pattern: re.Pattern = re.compile(r'(?<=-)(.*?)(?=-)')
+def rename_samples(path_to_audio_files, instrument: str) -> None:
+    """
+    :param path_to_audio_files:
+    :return:
+    """
+
+    nsynth_midi_note_pattern: re.Pattern = re.compile(r'(?<=-)(.*?)(?=-)')
+    samples: List[str] = os.listdir(path_to_audio_files)
     midi_note_table = MidiNoteTable()
 
-    print(midi_note_table.number_to_note(0))
+    # Loop through all the samples and rename with a note rather than a midi number representing a note.
+    for i, sample in enumerate(samples):
+        path_to_sample: str = join(path_to_audio_files, sample)
+        midi_note = int(nsynth_midi_note_pattern.findall(sample)[0])  # Number in range of 0-127
+        note: str = midi_note_table.number_to_note(midi_note)  # Actual note, example: D#
+        new_name: str = f'{instrument}_{note}_{str(i).zfill(6)}.wav'
+        new_name_path: str = join(path_to_audio_files, new_name)
+
+        os.rename(path_to_sample, new_name_path)
+
+def main():
+    path_to_data_set: str = '../../data-sets/nsynth'
+    ontology: List[str] = os.listdir(path_to_data_set)
+
+    for instrument in ontology:
+        path_to_instrument: str = join(path_to_data_set, instrument)
+        rename_samples(path_to_instrument, instrument)
+
 
 if __name__ == '__main__':
     main()
