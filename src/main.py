2b9471af6fa6f7562ef18f0724e0a0a821792f08
Andrea Spiteri <aspit002@gold.ac.uk>
2022-04-14 13:38:37 +0100

    Created a script to extract audio from neurons

--- src/main.py
+++ src/main.py
@@ -1,116 +1,104 @@
-from ai_tools.model_builders import build_conv2d_model, bayesian_optimization_test_model, vgg_like_model
-import settings as sett
-from ai_tools import ModelManager
+import os
+from tensorflow.keras.models import load_model, Model
+from keras.engine.keras_tensor import KerasTensor
 from typing import List
 import numpy as np
-import os
+from utils.audio_tools import load_and_convert_audio_into_mel_spectrogram
+import utils.constants as consts
+from librosa.feature.inverse import mel_to_audio
+from soundfile import write
 from os.path import join
-from dataclasses import dataclass
-from tensorflow.keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
-from random import shuffle
-from bayes_opt import SequentialDomainReductionTransformer, BayesianOptimization
-from tensorflow.keras.models import Model
+from librosa.display import specshow
+from librosa import power_to_db
+import matplotlib.pyplot as plt
+from concurrent.futures import ProcessPoolExecutor
+from utils.constants import X_SHAPE
 
 
-@dataclass
-class Data:
-    data: np.ndarray
-    label: np.ndarray
+PATH_TO_MODEL: str = '../models/model_0/epoch-26.pb'
+PATH_TO_AUDIO: str = '../media/audio/string_a#.wav'
+OUTPUT_DIR: str = '../layer_outputs'
+NUM_LAYERS: int = 44
+NUM_WORKERS: int = 16
+
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 
-def load_data(path_to_data_set: str):
+def save_neuron_output(layer_activation, neuron_i, path_to_out_dir, path_to_mel_specs):
     """
-    :param path_to_data_set:
+    :param layer_activation:
+    :param neuron_i:
+    :param path_to_out_dir:
+    :param path_to_mel_specs:
     :return:
     """
 
-    ontology: List[str] = os.listdir(path_to_data_set)
-    ontology_len: int = len(ontology)
+    print(f'Processing {neuron_i}')
 
-    data: List[Data] = []
+    mel_spec: np.ndarray = layer_activation[0, :, :, neuron_i]
+    pad_left: np.ndarray = X_SHAPE[0] - mel_spec.shape[0]
+    pad_right: np.ndarray = X_SHAPE[1] - mel_spec.shape[1]
+    mel_spec = np.pad(mel_spec, ((0, pad_left), (0, pad_right)), 'minimum')
 
-    for i, instrument in enumerate(ontology):
-        path_to_instrument: str = join(path_to_data_set, instrument)
-        samples: List[str] = os.listdir(path_to_instrument)
-
-        for sample in samples:
-            path_to_sample: str = join(path_to_instrument, sample)
-            mel_spec: np.ndarray = np.load(path_to_sample)
-            label: np.ndarray = to_categorical(i, ontology_len)
-
-            data.append(Data(mel_spec, label))
-
-    shuffle(data)
-
-    X_data: np.ndarray = np.stack([x.data for x in data])
-    y_data: np.ndarray = np.stack([y.label for y in data])
-
-    return X_data, y_data
-
-
-def main() -> None:
-    the_meaning_of_life: int = 42
-    training_batch_size: int = 64
-
-    X, y = load_data(join(sett.dataset_path, 'train'))
-    X_val, y_val = load_data(join(sett.dataset_path, 'validation'))
-    X_test, y_test = load_data(join(sett.dataset_path, 'test'))
-    num_classes: int = y.shape[-1]
-
-    # X, y = load_data(sett.dataset_path)  # type: np.ndarray, np.ndarray
-    # X, X_test, y, y_test = train_test_split(X, y, test_size=0.2)  # type: np.ndarray, np.ndarray, np.ndarray, np.ndarray
-    # X, X_val, y, y_val = train_test_split(X, y, test_size=0.2)  # type: np.ndarray, np.ndarray, np.ndarray, np.ndarray
-    # num_classes: int = y.shape[-1]
-
-    model_params = dict(
-        num_conv_block=9,
-        num_filters=128,
-        num_dense_layers=2,
-        dense_layer_units=64,
-        conv_dropout_amount=0.1,
-        num_classes=num_classes,
+    signal: np.ndarray = mel_to_audio(
+        mel_spec,
+        sr=consts.SAMPLE_RATE,
+        n_iter=consts.MEL_TO_AUDIO_N_ITERATIONS
     )
 
-    model_manager = ModelManager(
-        model_builder_func=vgg_like_model,
-        train_data=X,
-        validation_data=X_val,
-        test_data=X_test,
-        train_labels=y,
-        validation_labels=y_val,
-        test_labels=y_test,
-        path_to_logs=sett.logs_path,
-        model_checkpoint_dir=sett.model_checkpoint_path,
-        training_batch_size=training_batch_size
-    )
+    neuron_name: str = f'neuron_{neuron_i}'
+    write(join(path_to_out_dir, neuron_name) + '.wav', signal, consts.SAMPLE_RATE)
 
-    pbounds = dict(
-        num_first_conv_blocks=(1, 9),
-        num_second_conv_blocks=(1, 9),
-        num_third_conv_blocks=(1, 9),
-        num_fourth_conv_blocks=(1, 9),
-        dropout_amount=(0.0, 0.499),
-        learning_rate=(0.0, 0.0001),
-        num_classes=(y.shape[-1], y.shape[-1])
-    )
+    mel_spec_log: np.ndarray = power_to_db(mel_spec, ref=np.max)
+    specshow(mel_spec_log, sr=consts.SAMPLE_RATE)
+    plt.savefig(join(path_to_mel_specs, neuron_name + '.png'))
 
-    # Create optimizer object.
-    optimizer = BayesianOptimization(
-        f=model_manager.build_train_and_evaluate_model,
-        pbounds=pbounds,
-        random_state=the_meaning_of_life,
-        bounds_transformer=SequentialDomainReductionTransformer(),
-    )
 
-    optimizer.maximize(
-        init_points=15,
-        n_iter=50,
-    )
+def main():
+    input_data: np.ndarray = load_and_convert_audio_into_mel_spectrogram(PATH_TO_AUDIO, True, 1.0)
 
-    # accuracy: float = model_manager.build_train_and_evaluate_model(early_stopping_patience=15, **model_params)
+    print('Loading model.')
+    # Create model for feature visualization.
+    model: Model = load_model(PATH_TO_MODEL)
+    layer_output: List[KerasTensor] = [layer.output for layer in model.layers[:NUM_LAYERS]]
+    activation_model = Model(inputs=model.input, outputs=layer_output)
 
-    # print(f'Model accuracy: {accuracy}')
+    print('Gathering layers.')
+
+    # Collect indicis to model's conv block layers.
+    layers = activation_model.layers
+    conv_block_indicis: List[int] = []
+
+    for i, layer in enumerate(layers):
+        if 'conv_block' in layer.name:
+            conv_block_indicis.append(i)
+
+    activations: List[np.ndarray] = activation_model.predict(input_data)
+
+    print('Looping over layers.')
+
+    # Loop over all conv blocks.
+    for layer_i in conv_block_indicis:
+        layer_activation: np.ndarray = activations[layer_i]
+        layer_name: str = f'conv_block_{layer_i}'
+
+        path_to_out_dir: str = join(OUTPUT_DIR, layer_name)
+        path_to_mel_specs: str = join(path_to_out_dir, 'mel_specs')
+
+        os.makedirs(path_to_out_dir, exist_ok=True)
+        os.makedirs(path_to_mel_specs, exist_ok=True)
+
+        # Loop over each neuron and save audio.
+        for neuron_i in range(layer_activation.shape[-1] - 1):
+
+            with ProcessPoolExecutor(max_workers=NUM_WORKERS) as process_pool_executor:
+                process_pool_executor.submit(
+                    save_neuron_output,
+                    layer_activation,
+                    neuron_i,
+                    path_to_out_dir,
+                    path_to_mel_specs
+                )
 
 
 if __name__ == '__main__':
---
+++ src/main_training_script.py
@@ -0,0 +1,113 @@
+from ai_tools.model_builders import build_conv2d_model, bayesian_optimization_test_model, vgg_like_model
+import settings as sett
+from ai_tools import ModelManager
+from typing import List
+import numpy as np
+import os
+from os.path import join
+from dataclasses import dataclass
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from random import shuffle
+from bayes_opt import SequentialDomainReductionTransformer, BayesianOptimization
+from tensorflow.keras.models import Model
+
+
+@dataclass
+class Data:
+    data: np.ndarray
+    label: np.ndarray
+
+
+def load_data(path_to_data_set: str):
+    """
+    :param path_to_data_set:
+    :return:
+    """
+
+    ontology: List[str] = os.listdir(path_to_data_set)
+    ontology_len: int = len(ontology)
+
+    data: List[Data] = []
+
+    for i, instrument in enumerate(ontology):
+        path_to_instrument: str = join(path_to_data_set, instrument)
+        samples: List[str] = os.listdir(path_to_instrument)
+
+        for sample in samples:
+            path_to_sample: str = join(path_to_instrument, sample)
+            mel_spec: np.ndarray = np.load(path_to_sample)
+            label: np.ndarray = to_categorical(i, ontology_len)
+
+            data.append(Data(mel_spec, label))
+
+    shuffle(data)
+
+    X_data: np.ndarray = np.stack([x.data for x in data])
+    y_data: np.ndarray = np.stack([y.label for y in data])
+
+    return X_data, y_data
+
+
+def main() -> None:
+    the_meaning_of_life: int = 42
+    training_batch_size: int = 64
+
+    X, y = load_data(join(sett.dataset_path, 'train'))
+    X_val, y_val = load_data(join(sett.dataset_path, 'validation'))
+    X_test, y_test = load_data(join(sett.dataset_path, 'test'))
+    num_classes: int = y.shape[-1]
+
+
+    # model_params = dict(
+    #     num_conv_block=9,
+    #     num_filters=128,
+    #     num_dense_layers=2,
+    #     dense_layer_units=64,
+    #     conv_dropout_amount=0.1,
+    #     num_classes=num_classes,
+    # )
+
+    model_manager = ModelManager(
+        model_builder_func=vgg_like_model,
+        train_data=X,
+        validation_data=X_val,
+        test_data=X_test,
+        train_labels=y,
+        validation_labels=y_val,
+        test_labels=y_test,
+        path_to_logs=sett.logs_path,
+        model_checkpoint_dir=sett.model_checkpoint_path,
+        training_batch_size=training_batch_size
+    )
+
+    pbounds = dict(
+        num_first_conv_blocks=(1, 9),
+        num_second_conv_blocks=(1, 9),
+        num_third_conv_blocks=(1, 9),
+        num_fourth_conv_blocks=(1, 9),
+        dropout_amount=(0.0, 0.499),
+        learning_rate=(0.0, 0.0001),
+        num_classes=(y.shape[-1], y.shape[-1])
+    )
+
+    # Create optimizer object.
+    optimizer = BayesianOptimization(
+        f=model_manager.build_train_and_evaluate_model,
+        pbounds=pbounds,
+        random_state=the_meaning_of_life,
+        bounds_transformer=SequentialDomainReductionTransformer(),
+    )
+
+    optimizer.maximize(
+        init_points=15,
+        n_iter=50,
+    )
+
+    # accuracy: float = model_manager.build_train_and_evaluate_model(early_stopping_patience=15, **model_params)
+
+    # print(f'Model accuracy: {accuracy}')
+
+
+if __name__ == '__main__':
+    main()
